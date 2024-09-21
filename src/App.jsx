680bad45f1056a091670430099cbbaa14f010927
Sagar Tandan <076bct072@ioepc.edu.np>
2024-09-21 13:35:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <div class="w-full max-w-screen-2xl mx-auto bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+  );
+}
+
+export default App;
